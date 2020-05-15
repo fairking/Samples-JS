a63d46b84e863e5d04250e87ef259de82fea47e7
fairking <denis.pujdak@gmail.com>
2020-05-15 10:46:34 +0100

    odata sample (headers are not applied)

--- Vue.js/02. Working with report viewer/src/App.vue
+++ Vue.js/02. Working with report viewer/src/App.vue
@@ -8,38 +8,48 @@
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  data () {
-    return {
-    
-    }
+  name: "app",
+  data() {
+    return {};
   },
   mounted: function() {
-    console.log('Loading Viewer view');
-    
-    console.log('Creating the report viewer with default options');
-    var viewer = new window.Stimulsoft.Viewer.StiViewer(null, 'StiViewer', false);
+    console.log("Loading Viewer view");
 
-    console.log('Creating a new report instance');
-    
+    console.log("Creating the report viewer with default options");
+    var viewer = new window.Stimulsoft.Viewer.StiViewer(
+      null,
+      "StiViewer",
+      false
+    );
+
+    viewer.onBeginProcessData = this.onBeginProcessData;
+
+    console.log("Creating a new report instance");
+
     var report = new window.Stimulsoft.Report.StiReport();
 
-    console.log('Load report from url');
-    report.loadFile('/reports/SimpleList.mrt');
+    console.log("Load report from url");
+    report.loadFile("/reports/Report.mrt");
 
-    console.log('Assigning report to the viewer, the report will be built automatically after rendering the viewer');
+    console.log(
+      "Assigning report to the viewer, the report will be built automatically after rendering the viewer"
+    );
     viewer.report = report;
 
-    console.log('Rendering the viewer to selected element');
-    viewer.renderHtml('viewer');
+    console.log("Rendering the viewer to selected element");
+    viewer.renderHtml("viewer");
 
-    console.log('Loading completed successfully!');
+    console.log("Loading completed successfully!");
+  },
+  methods: {
+    onBeginProcessData(e) {
+      e.headers.push({ key: "Authorization", value: "Bearer 123456789" });
+      console.log(e);
+    }
   }
-}
+};
 </script>
 
 <style>
-
 </style>
