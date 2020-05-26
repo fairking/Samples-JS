3a1bf434db0898a50c92ec56b708a7c426d48960
fairking <denis.pujdak@gmail.com>
2020-05-26 10:08:37 +0100

    headers and dates test

--- Vue.js/01. Working with report designer/src/App.vue
+++ Vue.js/01. Working with report designer/src/App.vue
@@ -8,39 +8,60 @@
 </template>
 
 <script>
-
 export default {
-  name: 'app',
-  data () {
-    return {
-    
-    }
+  name: "app",
+  data() {
+    return {};
   },
   mounted: function() {
-    console.log('Loading Designer view');
+    console.log("Loading Designer view");
 
-    console.log('Set full screen mode for the designer');
+    console.log("Set full screen mode for the designer");
     var options = new window.Stimulsoft.Designer.StiDesignerOptions();
     options.appearance.fullScreenMode = false;
 
-    console.log('Create the report designer with specified options');
-    var designer = new window.Stimulsoft.Designer.StiDesigner(options, 'StiDesigner', false);
+    console.log("Create the report designer with specified options");
+    var designer = new window.Stimulsoft.Designer.StiDesigner(
+      options,
+      "StiDesigner",
+      false
+    );
 
-    console.log('Create a new report instance');
+    designer.onBeginProcessData = this.onBeginProcessData;
+
+    console.log("Create a new report instance");
     var report = new window.Stimulsoft.Report.StiReport();
 
-    console.log('Load report from url');
-    report.loadFile('reports/SimpleList.mrt');
+    console.log("Load report from url");
+    report.loadFile("reports/SimpleList.mrt");
 
-    console.log('Edit report template in the designer');
+    // https://services.odata.org/TripPinRESTierService/(S(vuzbl01wsrehumusahsgyqbl))/
+    // https://services.odata.org/TripPinRESTierService/(S(vuzbl01wsrehumusahsgyqbl))/People('scottketchum')/Trips
+    var db = new window.Stimulsoft.Report.Dictionary.StiODataDatabase(
+      "OpenData",
+      "odata",
+      "https://services.odata.org/TripPinRESTierService/(S(vuzbl01wsrehumusahsgyqbl))/",
+      false
+    );
+    report.dictionary.databases.add(db);
+
+    report.onBeginProcessData = this.onBeginProcessData;
+
+    console.log("Edit report template in the designer");
     designer.report = report;
 
-    console.log('Rendering the viewer to selected element');
-    designer.renderHtml('designer');
+    console.log("Rendering the viewer to selected element");
+    designer.renderHtml("designer");
 
-    console.log('Loading completed successfully!');
+    console.log("Loading completed successfully!");
+  },
+  methods: {
+    onBeginProcessData(e) {
+      e.headers.push({ key: "Authorization", value: "Bearer 123456789" });
+      console.log(e);
+    }
   }
-}
+};
 </script>
 
 <style>
